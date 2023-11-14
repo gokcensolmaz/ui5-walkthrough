abe7fd343fa1d6355629dcda475df2a9ad4693eb
gokcen.solmaz <gokcensolmaz99@gmail.com>
2023-11-14 17:21:21 +0300

    Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ReosurceModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onInit: function () {
@@ -15,12 +16,22 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
 
+            //set i18n model on view
+            var i18nModel = new ReosurceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello: function () {
-
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg",[sRecipient]);
 
             // Show a native or vanailla JS alert
-            MessageToast.show("Hello you")
+            MessageToast.show(sMsg);
         }
     });
 });
